6d4df7ddc30db8ae879aafbaafae9d26e81c0a54
Victor Rusakovich <nemiga@gmail.com>
2020-02-14 17:37:39 +0300

    Update to Google Sheet API v4

--- test/expected/amd/ru.sheet_first.js
+++ test/expected/amd/ru.sheet_first.js
@@ -1,9 +1,11 @@
 /* eslint quotes: 0 */
 define({
   "firstRow.title": "Список всех доступных комбинаций",
-  "number": 31337,
+  "number": "31337",
   "phone.startsWithPlus": "+375295551690",
   "string.zeros": "07931505",
   "empty": "",
-  "justSpaceChar": ""
+  "justSpaceChar": " ",
+  "afterEmptyRow.has.specialChars": "d/m \\\\a\\\\t; \\n // {{double brackets}}",
+  "toBeReplaced": "_replaced_"
 })
--- test/expected/esm/SECOND_SHEET.en.js
+++ test/expected/esm/SECOND_SHEET.en.js
@@ -1,9 +1,11 @@
 /* eslint quotes: 0 */
 export default {
   "firstRow.title": "List all possible combinations",
-  "number": 7350,
+  "number": "7350",
   "phone.startsWithPlus": "+1555785123",
   "string.zeros": "0000",
   "empty": "",
-  "justSpaceChar": ""
+  "justSpaceChar": " ",
+  "afterEmptyRow.has.specialChars": "d/m \\\\a\\\\t; \\n // {brackets}",
+  "toBeReplaced": "_replaced_"
 }
