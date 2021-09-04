2ee32f2ccb6f89e06a36c11ab553d90a29eac2d0
Victor Rusakovich <nemiga@gmail.com>
2021-09-04 20:49:10 +0300

    Clean example from typos

--- customOptions.default.js
+++ customOptions.default.js
@@ -6,8 +6,8 @@ module.exports = {
     }
     /* You can hardcode these values if your policies allow to store secrets in repo:
        return {
-         private_key:  "-----BEGIN PRIVATE KEY-----\nL3OEUJ5OYH7F02I6KUAY\n-----END PRIVATE KEY-----\n",,
-         client_email: "this-is-fake-@some-name-you-make.iam.gserviceaccount.com",
+         private_key:  "-----BEGIN PRIVATE KEY-----\nL3OEUJ5OYH7F02I6KUAY\n-----END PRIVATE KEY-----\n",
+         client_email: "this-is-fake@some-name-you-make.iam.gserviceaccount.com",
        }
     */
   },
