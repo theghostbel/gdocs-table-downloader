a0f3f56d1865d543a47c959a7a451a92d81b64dc
Victor Rusakovich <v.rusakovich@gpsolutions.com>
2021-09-07 00:05:53 +0300

    Support empty "key" cell
    (cherry picked from commit 80c6747255d267dc0a34c3c4901e360b1984fa2f)

---
+++ test/expected/amd/en.corrupted_sheet.js
@@ -0,0 +1,4 @@
+/* eslint quotes: 0 */
+define({
+  "it.misses.the.key.column.key": "Can't work without it"
+})
---
+++ test/expected/esm/corrupted_sheet.en.js
@@ -0,0 +1,4 @@
+/* eslint quotes: 0 */
+export default {
+  "it.misses.the.key.column.key": "Can't work without it"
+}
